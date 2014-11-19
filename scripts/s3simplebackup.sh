b553e9f1ba7b6154b785289905c5a82c6a43d6f0
Alex Kondratiev <xelak.kondratev@gmail.com>
2014-11-19 17:51:44 +0200

    Region was added

--- scripts/s3simplebackup.sh
+++ scripts/s3simplebackup.sh
@@ -6,17 +6,17 @@
 #/tmp/awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 
 DATE=`date +%Y-%m-%d`
-BACKUPDIR='/etc /ebs/www /ebs/www2' #Directories to be backed up
-STORAGEDIR='/ebs/backup' #A directory for TMP backups
+BACKUPDIR='/etc /var/www' #Directories to be backed up
+STORAGEDIR='/tmp' #A directory for TMP backups
 DOMAIN='' #Master domain of the client
 MYSQLDUMP='/usr/bin/mysqldump'
-MYSQLUSER=''
+MYSQLUSER='root'
 MYSQLPASS=''
 AWS='/usr/local/bin/aws'
 BUCKET='' #Client bucket at ITsyndicate S3
 
 #aes configuration
-export AWS_DEFAULT_REGION='us-west-2'
+export AWS_DEFAULT_REGION='eu-central-1'
 export AWS_ACCESS_KEY_ID=''
 export AWS_SECRET_ACCESS_KEY=''
 
@@ -27,4 +27,7 @@ $MYSQLDUMP --add-drop-database --add-drop-table --disable-keys --all-databases -
 $AWS s3 cp $STORAGEDIR/${DATE}_$DOMAIN.tar.gz s3://$BUCKET/${DATE}_$DOMAIN.tar.gz
 $AWS s3 cp $STORAGEDIR/${DATE}_$DOMAIN.sql.gz s3://$BUCKET/${DATE}_$DOMAIN.sql.gz
 
-/usr/bin/find $STORAGEDIR -type f -mtime +3 | /usr/bin/xargs /bin/rm -f >/dev/null 2>&1
+rm $STORAGEDIR/${DATE}_$DOMAIN.tar.gz $STORAGEDIR/${DATE}_$DOMAIN.sql.gz
+
+#Or you can use find option to keep local copy
+#/usr/bin/find $STORAGEDIR -type f -mtime +3 | /usr/bin/xargs /bin/rm -f >/dev/null 2>&1
